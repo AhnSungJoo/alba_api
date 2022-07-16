002c42e05ad42ae4ffdb67da97ed95910f541b74
potensj@gmail.com <harry@intelliquant.net>
2022-07-16 14:17:06 +0900

    push

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -43,9 +43,9 @@ router.post('/albaregister', async (ctx, next) => {
   const company_name = ctx.request.body.company_name; // 회사이름
   const review = ctx.request.body.review; // 후기
 
-  const alDAO = new albaDAO();
-  await alDAO.updateAlbaCompany(email, company_name);
-  await alDAO.updateAlbaReview(email, review);
+  // const alDAO = new albaDAO();
+  // await alDAO.updateAlbaCompany(email, company_name);
+  // await alDAO.updateAlbaReview(email, review);
 
   return ctx.redirect('/'); 
 });
